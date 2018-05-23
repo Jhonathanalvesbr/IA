bcf3c9a37dac71623d557527b644dd713aa7d0a9
Jhonathanalvesbr <Jhonathanalves_br@hotmail.com>
2018-05-23 11:21:21 -0300

    Update AG.c

--- AG.c
+++ AG.c
@@ -3,7 +3,7 @@
 #include<math.h>
 #include<conio.h>
 #include "Individuo.cpp"
-
+#include<time.h>
 
 double function1(double x) //Funcao de Aptdao
 {
@@ -13,9 +13,23 @@ double function1(double x) //Funcao de Aptdao
 	return x;
 }
 
+/*void calcularFit(int TamanhoCromossomo, int * Cromossomo)
+{
+	for(int i = 0; i < TamanhoCromossomo; i++)
+		{
+			function1()
+		}
+}*/
+
+struct individuo{
+	int individuo[5];
+	double fitness;
+	int decimal;
+};
 
 int main()
 {
+	srand(time(NULL));
 	int TamanhoCromossomo = 5; // Tamanho Individuo
 	int TamanhoPopulacao = 500; // Tamanho População
 	int X = pow(2,TamanhoCromossomo); // Funcao de 0 ate 2 exponencial cormossomo
@@ -24,17 +38,38 @@ int main()
 	int vet[500][5];
 	
 	
-	for(int x = 0; x < TamanhoPopulacao; x++)
+	for(int x = 0; x < 10; x++)
 	{
 		int *p = individuo(TamanhoCromossomo);
-		
+		printf("Binario: ");
 		for(int i = 0; i < TamanhoCromossomo; i++)
 		{
 			vet[x][i] = *(p+i);
 			printf("%i", vet[x][i]);
 		}
+		printf(" | Decimal: %i\n", getInt(vet[x]));
 		free(p);
-		puts("");
+		
+		int r = rand()%5;
+		if(r == 1)
+		{
+			printf("\tMultacao\n");
+			printf("Binario: ");
+			for(int i = 0; i < TamanhoCromossomo; i++)
+			{
+				printf("%i", vet[x][i]);
+			}
+			printf(" | Decimal: %i\n", getInt(vet[x]));
+			printf("Binario: ");
+			multarBit(vet[x],TamanhoCromossomo);
+			for(int i = 0; i < TamanhoCromossomo; i++)
+			{
+				printf("%i", vet[x][i]);
+			}
+			printf(" | Decimal: %i\n", getInt(vet[x]));
+			printf("\tMultacao\n");
+		}
+		
 	}
 	
 	
